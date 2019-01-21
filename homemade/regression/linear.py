5ffca46de31c3e53160499284083d05bf38b7699
dantin <chengjie.ding@gmail.com>
2019-01-21 14:02:04 +0800

    linear regression.

---
+++ homemade/regression/linear.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+""" Linear Regression."""
+
+
+import numpy as np
+
+
+class LinearRegression():
+    """Linear Regression Class."""
+
+    def __init__(self, data, labels, polynomial_degree=0, sinusoid_degree=0, normalize_data=True):
+        pass
